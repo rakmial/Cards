1b8110f0afff94280ff7cea82a5ae7404e09aca1
Joshua Lovelace <rakmial@gmail.com>
2020-09-01 10:49:47 -0700

    test 1 red

---
+++ lib/Card.rb
@@ -0,0 +1,6 @@
+require('rspec')
+require('pry')
+
+class Card
+
+end
---
+++ spec/Card_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('Card')
+
+describe("Card") do
+  it('initializes with suit from ["H", "S", "C", "D"] and rank from [2..10,"J","Q","K","A"], \
+  provides reader for each and @value for both.') do
+    expect(Card.new(2,"H").suit).to(eq("H"))
+    expect(Card.new(2,"H").rank).to(eq(2))
+    expect(Card.new(2,"H").value).to(eq("2H"))
+  end
+end
