d65d0145323be8b30bec7d9fb113711f15641bbf
Joshua Lovelace <rakmial@gmail.com>
2020-09-01 11:00:11 -0700

    test 1 green

--- lib/Card.rb
+++ lib/Card.rb
@@ -2,5 +2,10 @@ require('rspec')
 require('pry')
 
 class Card
-
+  attr_reader(:rank, :suit, :value)
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+    @value = rank.to_s + suit
+  end
 end
