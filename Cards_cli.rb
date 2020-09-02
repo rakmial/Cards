2027417514643265031077176739196d1429163a
Joshua Lovelace <rakmial@gmail.com>
2020-09-01 17:24:44 -0700

    .discard not quite working

--- Cards_cli.rb
+++ Cards_cli.rb
@@ -6,19 +6,34 @@ puts "You crack open a factory-fresh pack of cards."
 puts "The plastic smell is overwhelming, and the card"
 puts "edges are smooth and cool against your fingers."
 deck = Deck.new
+hand = Hand.new
 
 until game_over do
-  puts "Choose one: count, draw, shuffle, quit"
+  puts "Choose one: count, shuffle, draw, hand, quit"
   choice = gets.chomp
 
   if choice == "count"
     puts "There are " + deck.count.to_s + " cards left in the deck."
   elsif choice == "draw"
-    puts "You draw a " + deck.draw.name + " and fork it to /dev/null"
+    hand.draw(deck)
+    puts "You draw a " + hand.current_hand[-1].name + " and put it in your hand."
   elsif choice == "shuffle"
     deck = deck.shuffle
     puts "You masterfully shuffle and bridge the cards, somehow"
     puts "making them perfectly random in one go."
+  elsif choice == "hand"
+    puts "You have"
+    i = 0
+    hand.current_hand.each do |card|
+      i += 1
+      puts i.to_s + ": " + card.name
+    end
+    puts "Do you want to discard any? Enter no or a number between 1 and " + i.to_s
+    discarding = gets.chomp
+    if discarding == "no"
+      break
+    hand = hand.discard(current_hand[discarding.to_i-1].value)
+    end
   elsif choice == "quit"
     puts "Thanks for ... playing, I guess?"
     game_over = true
